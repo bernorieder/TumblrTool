480372b688d9ca70e72d43183422e65dede758de
Bernhard Rieder <rieder@wcw-staff-145-18-163-185.wireless.uva.nl>
2015-07-01 13:27:35 +0200

    new version

---
+++ conf_example.php
@@ -0,0 +1,11 @@
+<?php
+
+$base_url = "http://yourserver.com/yourdirectory/";
+
+// to get the following variables, register a client at http://instagram.com/developer/clients/register/
+
+$apiKey = "";
+$apiSecret = "";
+$apiCallback = "";			// must point to success.php and be identical to the redirect URI specified in the client manager on Instagram
+
+?>
