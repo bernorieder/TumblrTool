b0991743a60df93a9a51458e6ae4cfc7ca964192
Bernhard Rieder <rieder@wcw-staff-145-18-163-185.wireless.uva.nl>
2015-07-01 13:27:28 +0200

    new version

--- index.php
+++ index.php
@@ -1,99 +1,56 @@
 <?php
 
-// ----- conf -----
-$iterations = 50;			//	how many sets of 20 images to use
-
-
-// ----- load -----
-include "conf.php";
-
-
-// check query parameter
-if(!isset($_GET["query"])) {
-	echo "call the script with ?query=whatyouwanttoquery, e.g. https://lab.digitalmethods.net/~brieder/tumblr/tagnet/?query=yourtag"; exit;
-}
-$query = $_GET["query"];
-
-// api URL
-$url = "http://api.tumblr.com/v2/tagged?tag=".urlencode($query)."&limit=20&api_key=" . $api_key . "&before=";
-
-
-// retrieve $iterations	 packs of 20 images
-$results = array();
-$before = "";
-
-echo "(getting ".$iterations." iterations of 20 images) ";
-
-for($i = 0; $i < $iterations; $i++) {
-	$data = file_get_contents($url . $before);
-	$data = json_decode($data);
-
-	$before = $data->response[count($data->response) - 1]->timestamp;
-	$results = array_merge($results,$data->response);
-
-	echo $i . " "; flush(); ob_flush();
-	sleep(1);
-}
-
-
-// create graph
-$tags = array();
-$edges = array();
-
-foreach($results as $item) {
-
-	$tmptags = $item->tags;
-
-	// iterate over half of ajacency matrix
-	for($i = 0; $i < count($tmptags); $i++) {
-
-		$tmptags[$i] = strtolower($tmptags[$i]);
-		$tmptags[$i] = preg_replace("/,/", " ", $tmptags[$i]);
-
-		if(!isset($tags[$tmptags[$i]])) {
-			$tags[$tmptags[$i]] = 1;
-		} else {
-			$tags[$tmptags[$i]]++;
-		}
-
-		for($j = $i; $j < count($tmptags); $j++) {
-
-			$tmptags[$j] = strtolower($tmptags[$j]);
-			$tmptags[$j] = preg_replace("/,/", " ", $tmptags[$j]);
-
-			$tmpedge = array($tmptags[$i],$tmptags[$j]);
-			asort($tmpedge);
-			$tmpedge = implode("_|||_", $tmpedge);
-
-			if(!isset($edges[$tmpedge])) {
-				$edges[$tmpedge] = 1;
-			} else {
-				$edges[$tmpedge]++;
-			}
-		}
-	}
-}
-
-arsort($tags);
-
-
-// create output
-$gdf = "nodedef>name VARCHAR,label VARCHAR,count INT\n";
-foreach($tags as $key => $value) {
-	$gdf .= md5($key) . "," . $key . "," . $value . "\n";
-}
-
-$gdf .= "edgedef>node1 VARCHAR,node2 VARCHAR,weight INT\n";
-
-foreach($edges as $key => $value) {
-	$tmpedge = explode("_|||_", $key);
-	$gdf .= md5($tmpedge[0]) . "," . md5($tmpedge[1]) . "," . $value . "\n";
-}
-
-$filename = "tumblr_" . $query . "_" .date("Y_m_d-H_i_s") . ".gdf";
-
-file_put_contents($filename, $gdf);
-
-echo '<br /><br />your file: <a href="https://lab.digitalmethods.net/~brieder/tumblr/tagnet/'.$filename.'">'.$filename.'</a>';
+ini_set( 'default_charset', 'UTF-8' );
 
 ?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	
+	<title>TumblrTool</title>
+	
+	<link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+
+<body>
+
+	<h1>TumblerTool</h1>
+	
+	<p>This script retrieves posts tagged with a specific term from tumblr (the /tagged endpoint documented <a href="https://www.tumblr.com/docs/en/api/v2" target="_blank">here</a>) and creates:
+	<ul>	
+		<li>a tabular file containing basic descriptions of the retrieved posts</li>
+		<li>a co-tag file (GDF format) to analyze e.g. in <a href="http://gephi.org" target="_blank">gephi</a>;</li>
+	</ul>
+	</p>
+	
+	<!-- <p>Source code and some documentation are available <a href="https://github.com/bernorieder/instagram-tagnet" target="_blank">here</a>.</p> -->
+	
+	<hr />
+		
+	<p>Specify a tag and the number of media to retrieve:</p>
+
+	<table>
+		<form action="tagnet.php" method="get">
+			<tr>
+				<td>Tag:</td>
+				<td><input type="text" name="tag" /></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td>Iterations:</td>
+				<td><input type="text" name="iterations" max="100" value="1" /> </td>
+				<td>(max. 100, one iteration gets 20 items)</td>
+			</tr>
+			<tr>
+				<td>HTML output:</td>
+				<td><input type="checkbox" name="htmloutput" /></td>
+			<td>(adds HTML result tables in addition to the file exports)</td>
+			</tr>
+			<tr>
+				<td colspan="3"><input type="submit" /></td>
+			</tr>
+		</form>
+	<table>
