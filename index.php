e14fda36b7c29c42df1f72ca64890dc1ec4b2687
Bernhard Rieder <rieder@MyMBA.local>
2015-07-02 08:08:19 +0200

    added field

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ ini_set( 'default_charset', 'UTF-8' );
 	
 	<p>This script retrieves posts tagged with a specific term from tumblr (the /tagged endpoint documented <a href="https://www.tumblr.com/docs/en/api/v2" target="_blank">here</a>) and creates:
 	<ul>	
-		<li>a tabular file containing basic descriptions of the retrieved posts</li>
+		<li>a tabular file containing basic descriptions of the retrieved posts;</li>
 		<li>a co-tag file (GDF format) to analyze e.g. in <a href="http://gephi.org" target="_blank">gephi</a>;</li>
 	</ul>
 	</p>
@@ -50,7 +50,21 @@ ini_set( 'default_charset', 'UTF-8' );
 			<td>(adds HTML result tables in addition to the file exports)</td>
 			</tr>
 			<tr>
+				<td>Show images:</td>
+				<td>
+					<select name="showimages">
+						<option value="off">no preview</option>
+						<option value="200">width: 200px</option>
+						<option value="500">width: 500px</option>
+						<option value="original">original size</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
 				<td colspan="3"><input type="submit" /></td>
 			</tr>
 		</form>
-	<table>
+	</table>
+
+</body>
+</html>
