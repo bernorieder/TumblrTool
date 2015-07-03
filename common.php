86f1f3b202f6e082e54d8a2565624c7fcb6882e7
Bernhard Rieder <rieder@MyMBA.local>
2015-07-03 08:49:12 +0200

    housekeeping

--- common.php
+++ common.php
@@ -24,6 +24,42 @@ function doAPIRequest($url) {
 			sleep(1);
 		}
 	}
-}		
+}
+
+
+function zipit($filename,$files) {
+
+	echo '<p>Compressing files...</p>'; flush(); ob_flush();
+
+	$zip = new ZipArchive();
+	$filename = $filename . ".zip";
+
+	if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+    	exit("cannot open <$filename>\n");
+	}
+
+	foreach ($files as $file) {
+		$cleanfile = preg_replace("/\.\/data\//", "", $file);		// cleaning up the filename to counter uncompress problems (with "." maybe?)
+		$zip->addFile($file,$cleanfile);
+		echo $cleanfile . "<br />";
+	}
+
+	echo '<p>Your files have been generated. ' . $zip->numFiles . ' files were zipped. ';
+	echo 'Download the <a href="'.$filename.'">zip archive</a>.</p>';
+
+	$zip->close();
+
+	foreach ($files as $file) {
+		unlink($file);
+	}
+}
+
+
+function clean($string) {
 	
+	$string = preg_replace("/[\n\r\t]/", " ", $string);
+	
+	return $string;
+}
+
 ?>
